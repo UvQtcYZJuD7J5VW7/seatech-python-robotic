6c3542d54863cf7fe1e597537d6023c154cd92e4
UvQtcYZJuD7J5VW7 <adrien.delepiere01@gmail.com*>
2023-01-05 16:56:09 +0100

    Accessors and functions

--- Exo1/main.py
+++ Exo1/main.py
@@ -4,20 +4,37 @@ class Robot():
     """
 
     # Attributes
-    __slots__ = (
-        "__name",
-        "__power",
-        "__current_speed",
-        "__battery_level",
-        "__states"
-    )
+    # __slots__ = (
+    #     "__name",
+    #     "__power",
+    #     "__current_speed",
+    #     "__battery_level",
+    #     "__states"
+    # )
     __name = "<unnamed>"
     __power = False
     __current_speed = 0
     __battery_level = 0
     __states = ['shutown', 'running']
+
+    # Accessors
+    def getBatteryLevel(self):
+        return self.__battery_level
+
+    def setBatteryLevel(self, battery_level):
+        self.__battery_level = battery_level
     
+    # Methods
+    def start(self):
+        pass
+
+    def shutdown(self):
+        pass
+
+    def charge(self, battery_level):
         
-    """
-      Give your best code here ( •̀ ω •́ )✧
-    """
+        pass
+
+robot = Robot()
+
+print(robot.getBatteryLevel())
