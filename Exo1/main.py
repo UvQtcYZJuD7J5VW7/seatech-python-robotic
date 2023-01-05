06c97ae62854f72dbf41c97499df58ca743f7c63
UvQtcYZJuD7J5VW7 <adrien.delepiere01@gmail.com*>
2023-01-05 16:40:01 +0100

    Copier coller du template

---
+++ Exo1/main.py
@@ -0,0 +1,23 @@
+class Robot():
+    """
+    Documentation
+    """
+
+    # Attributes
+    __slots__ = (
+        "__name",
+        "__power",
+        "__current_speed",
+        "__battery_level",
+        "__states"
+    )
+    __name = "<unnamed>"
+    __power = False
+    __current_speed = 0
+    __battery_level = 0
+    __states = ['shutown', 'running']
+    
+        
+    """
+      Give your best code here ( •̀ ω •́ )✧
+    """
