41a60f0383582fa0caeb7318c35248384d19699d
UvQtcYZJuD7J5VW7 <adrien.delepiere01@gmail.com*>
2023-01-13 11:46:13 +0100

    dsfs

--- exo3/main.py
+++ exo3/main.py
@@ -1,14 +1,71 @@
-from abc import ABCMeta
+from abc import ABCMeta, abstractmethod
 
-class Robot(metaclass=ABCMeta):
-    
-    # Attributes
-    __name = "<unnamed>"
+class UnmannedVehicle(metaclass=ABCMeta):
+    """
+    Abstract class, must be redifined
+    """
 
     # Methods
     @property
     @abstractmethod
-    def getName() -> str:
-        return __name
+    def getState() -> bool:
+        """
+        False = Shutdown
+        True = Started
+        """
+        pass
 
-class 
+    @abstractmethod
+    def startup() -> bool:
+        """
+        Start the vehicle
+        No args, return True if start successful
+        """
+        pass
+
+    @abstractmethod
+    def stop() -> bool:
+        """
+        Stop the vehicle
+        No args, return True if stop successful
+        """
+        pass
+
+class UAV(UnmannedVehicle):
+    """Unmanned Aerial Vehicle"""
+
+    pass
+
+class UUV(UnmannedVehicle):
+    """Unmanned Undersea Vehicle"""
+     # Attributes
+    __seaSpeed = 0
+    
+    # Methods
+    def pitch(pitch:int) -> None:
+        if(pitch < 0):
+            print("UUV en descente")
+        elif(pitch = 0):
+            print("UUV stabilisé")
+        elif(pitch > 0):
+            print("UUV en montée")
+        else:
+            print("Comportement indéfini")
+
+class UGV(UnmannedVehicle):
+    """Unmanned Ground Vehicle"""
+    pass
+
+if __name__ == "__main__":
+        
+    uav = UAV()
+    uav.do_something_interesting()
+    uav.do_something_aerial_specific()
+
+    ugv = UGV()
+    ugv.do_something_interesting()
+    ugv.do_something_ground_specific()
+
+    uuv = UUV()
+    uuv.do_something_interesting()
+    uuv.do_something_undersea_specific()
