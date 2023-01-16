74ace6a506114f706933cd9517e0a6e5fd5b1582
UvQtcYZJuD7J5VW7 <adrien.delepiere01@gmail.com*>
2023-01-16 09:03:10 +0100

    fgh

--- exo3/main.py
+++ exo3/main.py
@@ -14,70 +14,68 @@ class UnmannedVehicle(metaclass=ABCMeta):
     def startup(self):
         pass
 
+    def startupGeneric(self):
+        self.__state = True
+        self.startup()
+
     @abstractmethod
     def stop(self):
         pass
 
+    def stopGeneric(self):
+        self.__state = False
+        self.stop()
+
 class UAV(UnmannedVehicle):
     def startup(self):
-        self.__state = True
         print("Pompe électrique ON")
         print("Plein riche")
         print("Démarreur 5 sec")
         print("Moteur démarré")
 
     def stop(self):
-        self.__state = False
         print("Régime 1200 trs/min")
         print("Plein pauvre")
         print("Moteur arrrêté")
 
 class UUV(UnmannedVehicle):
     def startup(self):
-        self.__state = True
         print("Ecoutilles fermées")
         print("Démarreur 5 sec")
         print("Moteur démarré")
 
     def stop(self):
-        self.__state = False
         print("Ecoutilles ouvertes")
         print("Retirer les clefs")
         print("Moteur arrrêté")
 
 class UGV(UnmannedVehicle):
     def startup(self):
-        self.__state = True
         print("Démarreur 5 sec")
         print("Moteur démarré")
 
     def stop(self):
-        self.__state = False
         print("Retirer les clefs")
         print("Moteur arrrêté")
 
 if __name__ == "__main__":
         
     uav = UAV()
-    uav.startup()
-    uav.stop()
+    uav.startupGeneric()
+    uav.stopGeneric()
 
     print("\n")
 
     ugv = UGV()
-    ugv.startup()
-    ugv.stop()
+    ugv.startupGeneric()
+    ugv.stopGeneric()
 
     print("\n")
 
     uuv = UUV()
-    uuv.startup()
-    uuv.stop()
-    if(uuv.getState == True)
-    {
-        print("Moteur allumé")
-    }
-    else
-    {
-        print("Moteur éteindre")
-    }
+    uuv.startupGeneric()
+    if(uuv.getState == True):
+        print("--> Moteur allumé")
+    else:
+        print("--> Moteur éteindre")
+    uuv.stopGeneric()
