72a9fee8c3201ad9b892c9ba8e2772e8092589e4
UvQtcYZJuD7J5VW7 <adrien.delepiere01@gmail.com*>
2023-01-27 10:08:22 +0100

    f

--- webots/controllers/my_controller/WarriorRobot.py
+++ webots/controllers/my_controller/WarriorRobot.py
@@ -5,28 +5,28 @@ class Engine():
     def __init__(self) -> None:
         self.back_left_wheel = Motor("back_left_wheel_joint")
         self.back_left_wheel.setPosition(float("inf"))
-        self.back_left_wheel.setVelocity(0.0)
+        self.back_left_wheel.setVelocity(0)
         self.back_right_wheel = Motor("back_right_wheel_joint")
         self.back_right_wheel.setPosition(float("inf"))
-        self.back_right_wheel.setVelocity(0.0)
+        self.back_right_wheel.setVelocity(0)
         self.front_left_wheel = Motor("front_left_wheel_joint")
         self.front_left_wheel.setPosition(float("inf"))
-        self.front_left_wheel.setVelocity(0.0)
+        self.front_left_wheel.setVelocity(0)
         self.front_right_wheel = Motor("front_right_wheel_joint")
         self.front_right_wheel.setPosition(float("inf"))
-        self.front_right_wheel.setVelocity(0.0)
+        self.front_right_wheel.setVelocity(0)
 
     def moveForward(self, velocity:int):
-        self.back_left_wheel(velocity)
-        self.back_right_wheel(velocity)
-        self.front_left_wheel(velocity)
-        self.front_right_wheel(velocity)
+        self.back_left_wheel.setVelocity(velocity)
+        self.back_right_wheel.setVelocity(velocity)
+        self.front_left_wheel.setVelocity(velocity)
+        self.front_right_wheel.setVelocity(velocity)
 
-    def moveLeft(self, velocity: int):
-        self.back_left_wheel(velocity)
-        self.back_right_wheel(-velocity)
-        self.front_left_wheel(-velocity)
-        self.front_right_wheel(velocity)
+    def moveLeft(self, velocity:int):
+        self.back_left_wheel.setVelocity(-velocity/2)
+        self.back_right_wheel.setVelocity(velocity)
+        self.front_left_wheel.setVelocity(-velocity/2)
+        self.front_right_wheel.setVelocity(velocity)
 
 class Sensors():
     def __init__(self, samplingPeriod) -> None:
@@ -35,6 +35,8 @@ class Sensors():
         self.__lidarFront.enablePointCloud()
         self.__radarFront = Radar("radar", samplingPeriod)
         self.__radarFront.enable(samplingPeriod)
+        self.__gps = GPS("gps")
+        self.__gps.enable(samplingPeriod)
     
     def lidarScan(self):
         list = self.__lidarFront.getRangeImage()
@@ -47,6 +49,32 @@ class Sensors():
     def get_targets(self):
         return self.__radarFront.getNumberOfTargets()
 
+    def checkCoordinates(self):
+        # Settings
+        corner = {
+            "corner1": (-3.5, -3.5),
+            "corner2": (-3.5, 3.5),
+            "corner3": (3.5, -3.5),
+            "corner4": (3.5, 3.5),
+        }
+        border = {
+            "left": -3.5,
+            "right": 3.5,
+            "front": 3.5,
+            "back": -3.5
+        }
+        limit = 0.3
+
+        selfCoordinates = self.__gps.getValues()
+        distances = []
+        for key in border.items():
+            distances.append(abs(selfCoordinates[0]-border[key]))
+            distances.append(abs(selfCoordinates[1]-border[key]))
+        distance = min(distances)
+        if(distance < limit):
+            print("Alerte")
+        print(distance)
+
 class WarriorRobot(Robot):
     def __init__(self):
         super().__init__()
@@ -57,4 +85,6 @@ class WarriorRobot(Robot):
         # if(self.sensors.getTargets() != 0):
         #     print("On bouge")
         # print(self.sensors.get_targets())
-        self.engine.moveForward(5)
+        self.sensors.checkCoordinates()
+        # self.engine.moveForward(5)
+        # self.engine.moveForward(5)
