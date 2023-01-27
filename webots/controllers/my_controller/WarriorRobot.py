4f1ede5e30479f52a786cc4116939efa80f547f4
UvQtcYZJuD7J5VW7 <adrien.delepiere01@gmail.com*>
2023-01-27 09:05:52 +0100

    sf

--- webots/controllers/my_controller/WarriorRobot.py
+++ webots/controllers/my_controller/WarriorRobot.py
@@ -3,26 +3,30 @@ from math import *
 
 class Engine():
     def __init__(self) -> None:
-        self.leftWheel = Motor("back_left_wheel_joint")
-        self.leftWheel.setPosition(float("inf"))
-        self.leftWheel.setVelocity(0.0)
-        self.rightWheel = Motor("back_right_wheel_joint")
-        self.rightWheel.setPosition(float("inf"))
-        self.rightWheel.setVelocity(0.0)
-        self.centerWheel = Motor("front_left_wheel_joint")
-        self.centerWheel.setPosition(float("inf"))
-        self.centerWheel.setVelocity(0.0)
-        self.centerWheel = Motor("front_right_wheel_joint")
-        self.centerWheel.setPosition(float("inf"))
-        self.centerWheel.setVelocity(0.0)
+        self.back_left_wheel = Motor("back_left_wheel_joint")
+        self.back_left_wheel.setPosition(float("inf"))
+        self.back_left_wheel.setVelocity(0.0)
+        self.back_right_wheel = Motor("back_right_wheel_joint")
+        self.back_right_wheel.setPosition(float("inf"))
+        self.back_right_wheel.setVelocity(0.0)
+        self.front_left_wheel = Motor("front_left_wheel_joint")
+        self.front_left_wheel.setPosition(float("inf"))
+        self.front_left_wheel.setVelocity(0.0)
+        self.front_right_wheel = Motor("front_right_wheel_joint")
+        self.front_right_wheel.setPosition(float("inf"))
+        self.front_right_wheel.setVelocity(0.0)
 
     def moveForward(self, velocity:int):
-        self.leftWheel(velocity)
-        self.rightWheel(velocity)
+        self.back_left_wheel(velocity)
+        self.back_right_wheel(velocity)
+        self.front_left_wheel(velocity)
+        self.front_right_wheel(velocity)
 
     def moveLeft(self, velocity: int):
-        self.leftWheel(-velocity)
-        self.rightWheel(velocity)
+        self.back_left_wheel(velocity)
+        self.back_right_wheel(-velocity)
+        self.front_left_wheel(-velocity)
+        self.front_right_wheel(velocity)
 
 class Sensors():
     def __init__(self, samplingPeriod) -> None:
@@ -30,6 +34,7 @@ class Sensors():
         # self.__lidarFront.fov = 1.5
         self.__lidarFront.enablePointCloud()
         self.__radarFront = Radar("radar", samplingPeriod)
+        self.__radarFront.enable(samplingPeriod)
     
     def lidarScan(self):
         list = self.__lidarFront.getRangeImage()
@@ -39,18 +44,17 @@ class Sensors():
         # # print(max(list))
         print("{}".format(list))
 
-    def getTargets(self):
-        return self.__radarFront.getTargets()
+    def get_targets(self):
+        return self.__radarFront.getNumberOfTargets()
 
 class WarriorRobot(Robot):
-
-    # Methods
     def __init__(self):
         super().__init__()
         self.engine = Engine()
         self.sensors = Sensors(int(self.getBasicTimeStep()))
 
     def run(self):
-        if(self.sensors.getTargets() != []):
-            print("On bouge")
-        self.sensors.getTargets()
+        # if(self.sensors.getTargets() != 0):
+        #     print("On bouge")
+        # print(self.sensors.get_targets())
+        self.engine.moveForward(5)
